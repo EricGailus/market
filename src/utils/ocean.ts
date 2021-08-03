fd2902653dde2adc15524f1969440a2b7327118d
Eric Gailus <e.gailus@sap.com>
2021-08-03 12:22:40 +0200

    add rinkeby contract addresses
    added current rinkeby contract addresses

--- src/utils/ocean.ts
+++ src/utils/ocean.ts
@@ -17,6 +17,16 @@ export function getOceanConfig(network: string | number): ConfigHelperConfig {
       : process.env.GATSBY_INFURA_PROJECT_ID
   )
 
+  if (config !== null && network === 4) {
+    config.oceanTokenAddress = '0x8967BCF84170c91B0d24D4302C2376283b0B3a07'
+    config.factoryAddress = '0x3fd7A00106038Fb5c802c6d63fa7147Fe429E83a'
+    config.metadataContractAddress =
+      '0xFD8a7b6297153397B7eb4356C47dbd381d58bFF4'
+    config.fixedRateExchangeAddress =
+      '0xeD1DfC5F3a589CfC4E8B91C1fbfC18FC6699Fbde'
+    config.poolFactoryAddress = '0x53eDF9289B0898e1652Ce009AACf8D25fA9A42F8'
+  }
+
   return config as ConfigHelperConfig
 }
 
